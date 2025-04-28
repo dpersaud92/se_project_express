2b632fc09920cbcbce5444d644c0f7d0d64b7031
dpersaud92 <dxp1020@gmail.com>
2025-04-27 22:06:29 -0400

    Updated

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended"],
+  rules: {},
+};
--- utils/errors.js
+++ utils/errors.js
@@ -1,9 +1,3 @@
-const BAD_REQUEST = 400;
-const NOT_FOUND = 404;
-const SERVER_ERROR = 500;
-
-module.exports = {
-  BAD_REQUEST,
-  NOT_FOUND,
-  SERVER_ERROR,
-};
+export const BAD_REQUEST = 400;
+export const NOT_FOUND = 404;
+export const SERVER_ERROR = 500;
