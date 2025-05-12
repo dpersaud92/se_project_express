99f5039b8673bc2529341f9419ed284a3128d2c3
dpersaud92 <dxp1020@gmail.com>
2025-05-12 12:09:00 -0400

    Helmet updated

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getCurrentUser, updateUser } from "../controllers/users.js";
+import auth from "../middlewares/auth.js";
 
 const router = Router();
 
-router.get("/me", getCurrentUser);
-router.patch("/me", updateUser);
+router.get("/me", auth, getCurrentUser);
+router.patch("/me", auth, updateUser);
 
 export default router;
